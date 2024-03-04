a89a329b84130cc649ea7faa129c7a1bc48117f3
Yiyang Wu <toolmanp@outlook.com>
2024-03-04 22:56:07 +0800

    fix: add load_dotenv in manage.py
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import os
 import sys
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
+    load_dotenv()
 
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
