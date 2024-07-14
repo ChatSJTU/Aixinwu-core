c7afa30b5dcd52052ddf5d2e3339d19d1c027d4d
Yiyang Wu <toolmanp@tlmp.cc>
2024-07-14 15:21:33 +0800

    feat(donation): migrate donations
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- saleor/donation/models.py
+++ saleor/donation/models.py
@@ -1,27 +1,42 @@
 from django.db import models
 from saleor import settings
+from django.utils import timezone
 from django_prices.models import MoneyField
 from django.conf import settings
 from saleor.account.models import User
 from saleor.permission.enums import DonationPermissions
+from . import DonationStatus
 import uuid
 
 
 class Donation(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    donator = models.ForeignKey(User, on_delete=models.CASCADE)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+    )
+    donator = models.CharField(max_length=128, null=True, blank=True)
+    barcode = models.CharField(max_length=256, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(default=timezone.now, editable=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
-    title = models.CharField(max_length=128)
-    description = models.TextField(blank=True)
-    currency = models.CharField(max_length=settings.DEFAULT_CURRENCY_CODE_LENGTH)
+    title = models.CharField(max_length=128, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    currency = models.CharField(
+        max_length=settings.DEFAULT_CURRENCY_CODE_LENGTH,
+        null=True,
+        blank=True,
+    )
     price_amount = models.DecimalField(
         max_digits=settings.DEFAULT_MAX_DIGITS,
         decimal_places=settings.DEFAULT_DECIMAL_PLACES,
         blank=True,
         null=True,
+    )
+    status = models.CharField(
+        max_length=32,
+        default=DonationStatus.UNREVIEWED,
+        choices=DonationStatus.CHOICES,
+        null=True,
+        blank=True,
     )
     price = MoneyField(amount_field="price_amount", currency_field="currency")
     quantity = models.PositiveIntegerField(default=1)
