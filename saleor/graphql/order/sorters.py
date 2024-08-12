fff1a783721187897e04d983b74a5787fafac91f
Yiyang Wu <toolmanp@tlmp.cc>
2024-08-12 19:09:38 +0800

    feat: use two fields for number sorting
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- saleor/graphql/order/sorters.py
+++ saleor/graphql/order/sorters.py
@@ -7,7 +7,7 @@ from ..core.types import BaseEnum, SortInputObjectType
 
 
 class OrderSortField(BaseEnum):
-    NUMBER = ["number"]
+    NUMBER = ["created_at", "number"]
     RANK = ["search_rank", "id"]
     CREATION_DATE = ["created_at", "status", "pk"]
     CREATED_AT = ["created_at", "status", "pk"]
